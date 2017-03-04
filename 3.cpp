884cba4980b5565d039aa6e8fdf5479d768b5dd9
Hazel Andreah Alemania <hvalemania@users.noreply.github.com>
2017-03-04 12:06:10 +0800

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -14,14 +14,13 @@ struct Tree *newTreeNode(int data) {
 	node->left = NULL;
 	node->right = NULL;
 	node->parent = NULL;
+
 	return node;
 }
 
 struct Tree* insertTreeNode(struct Tree *node, int data){
 	static Tree *p;
 	Tree *retNode;
-
-	if(node != NULL) p = node;
 
 	if(node == NULL)  {
 	    retNode = newTreeNode(data);
@@ -31,27 +30,31 @@ struct Tree* insertTreeNode(struct Tree *node, int data){
 	if(data <= node->data ) { 
 	    p = node;
 	    node->left = insertTreeNode(node->left,data);
-	} else {
+	} 
+	else {
 	    p = node;
 	    node->right = insertTreeNode(node->right,data);
 	} 
 	return node;
 }
 
-void printTreeInOrder(struct Tree *node) {
+void printTreeInOrder(struct Tree *node){
 	if(node == NULL) return;
+
 	printTreeInOrder(node->left);
 	cout << node->data << " ";
 	printTreeInOrder(node->right);
 }
 
-void printTreePostOrder(struct Tree *node) {
+void printTreePostOrder(struct Tree *node){
 	if(node == NULL) return;
+
 	printTreePostOrder(node->left);
 	printTreePostOrder(node->right);
 	cout << node->data << " ";
 }
-int main() {
+
+int main(int argc, char **argv){
 	Tree *root = newTreeNode('F');
 	insertTreeNode(root,'B');
 	insertTreeNode(root,'A');
@@ -61,9 +64,14 @@ int main() {
 	insertTreeNode(root,'G');
 	insertTreeNode(root,'I');
 	insertTreeNode(root,'H');
-	cout << "in order\n";
+
+	cout << "increasing sort order\n";
 	printTreeInOrder(root);
 	cout << endl;
+
 	cout << "post order \n";
 	printTreePostOrder(root);
+	cout << endl;
+
+	return 0;
 }
